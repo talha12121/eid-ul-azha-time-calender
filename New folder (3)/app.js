1095c983e11a5e63ba38a2b10b51ab0e8c4fbd77
Muhammad Talha <92953591+talha12121@users.noreply.github.com>
2022-07-05 22:32:41 +0500

    Add files via upload

---
+++ New folder (3)/app.js
@@ -0,0 +1,27 @@
+// var date = new Date()
+// var todaydate = new Date(date).getTime()
+// var newdate = new Date("07/10/2022").getTime()
+// var diff = newdate - todaydate
+// var total = Math.ceil(diff/(1000*24*60*60))
+// document.getElementById("card_1").innerHTML= "عید کے پہلے دن میں" + total+ "دن باقی ہیں";
+
+
+// var date = new Date()
+// var todaydate = new Date(date).getTime()
+// var newdate = new Date("07/11/2022").getTime()
+// var diff = newdate - todaydate
+// var total = Math.ceil(diff/(1000*24*60*60))
+// document.getElementById("card_2").innerHTML= "عید کے پہلے دن میں" + total+ "دن باقی ہیں";
+
+
+// var date = new Date()
+// var todaydate = new Date(date).getTime()
+// var newdate = new Date("07/12/2022").getTime()
+// var diff = newdate - todaydate
+// var total = Math.ceil(diff/(1000*24*60*60))
+// document.getElementById("card_3").innerHTML= "عید کے پہلے دن میں" + total + "دن باقی ہیں";
+
+
+
+
+
